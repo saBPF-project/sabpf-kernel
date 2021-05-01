85b45ed6b075c2e22fb23d01b0ddcef82f70a5c2
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2021-05-01 16:06:14 +0100

    script to install dependencies on fedora

---
+++ fedora-dependency.sh
@@ -0,0 +1,26 @@
+sudo dnf -y -v groupinstall 'Development Tools'
+sudo dnf -y -v install fedpkg fedora-packager rpmdevtools ncurses-devel
+sudo dnf -y -v install cmake clang gcc-c++ wget git pesign grubby
+sudo dnf -y -v install openssl-devel bc nano patch mosquitto sparse
+sudo dnf -y -v install flawfinder
+sudo dnf -y -v install bison flex
+sudo dnf -y -v install uthash-devel
+sudo dnf -y -v install inih-devel
+sudo dnf -y -v install paho-c-devel
+sudo dnf -y -v install uncrustify
+sudo dnf -y -v install rpm-build redhat-rpm-config
+sudo dnf -y -v install coccinelle
+sudo dnf -y -v install phoronix-test-suite
+sudo dnf -y -v install automake
+sudo dnf -y -v install alien
+sudo dnf -y -v install dwarves
+sudo dnf -y -v install python3.8 python3-devel
+sudo dnf -y -v install llvm elfutils-libelf elfutils-libelf-devel
+sudo dnf -y -v install libcap
+sudo dnf -y -v install openssl
+sudo dnf -y -v install valgrind
+sudo dnf -y -v install binutils-devel
+sudo dnf -y -v install readline-devel
+sudo dnf -y -v install libcap-devel
+sudo dnf -y -v install ruby
+sudo dnf -y -v install libbpf
